f2cf4981bc1ede7286e244e613cb79e3add61936
ManuRodriguezC <estebanclimb@gmail.com>
2022-07-21 11:31:04 -0500

    one more changes

--- get_op_functions.c
+++ get_op_functions.c
@@ -18,9 +18,10 @@ int (*get_op_functions(char c))(char *, va_list, int *)
 			{NULL, NULL}
 	};
 	int i = 0;
+
 	while (ops[i].op != '\0')
 		if (*ops[i].op == c)
 			return (ops[i].f);
-		i++;
+	i++;
 	return (NULL);
 }
